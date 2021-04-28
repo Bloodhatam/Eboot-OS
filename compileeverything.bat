bd0ca5bcd93d5541f9b2cca64d13bb546231c7fe
Bloodhatam <65913180+Bloodhatam@users.noreply.github.com>
2021-04-28 10:42:19 +0200

    Update and rename compileeverything.bat to how to compile.txt

--- compileeverything.bat
+++
@@ -1,5 +0,0 @@
-E:\Eboot\OS\i686-elf-tools-windows\bin\i686-elf-as.exe boot.s -o boot.o
-E:\Eboot\OS\i686-elf-tools-windows\bin\i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-E:\Eboot\OS\i686-elf-tools-windows\bin\i686-elf-gcc -T linker.ld -o ebootstd.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-
-pause
