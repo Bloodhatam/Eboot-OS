dad7bfeeb5ae781266320ca54ed01c957797ef6b
Bloodhatam <kubalukasik2007@outlook.com>
2021-05-02 18:54:10 +0200

    enable protected mode

--- boot.s
+++ boot.s
@@ -75,7 +75,16 @@ _start:
 	C++ features such as global constructors and exceptions will require
 	runtime support to work as well.
 	*/
- 
+	; set PE bit
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    ; far jump (cs = selector of code segment)
+    jmp cs:@pm
+
+    @pm:
+    ; Now we are in PM.
 	/*
 	Enter the high-level kernel. The ABI requires the stack is 16-byte
 	aligned at the time of the call instruction (which afterwards pushes
